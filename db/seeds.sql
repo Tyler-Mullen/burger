cdfc9b331a076cec8ea6689d04f4790896de8deb
RatofWrath <T_mullen93@yahoo.com>
2019-04-28 13:29:19 -0500

    made some updates

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Bacon Cheeseburger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('California Burger', 0);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Breakfast Burger', 0);
